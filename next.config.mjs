5a9c35b2d9cb87e750da3c3b77e933150063c0fd
Rein van Haaren <rein@vanhaaren.nl>
2024-09-16 14:22:50 +0200

    feat: track pageviews

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    after: true,
+  },
   logging: {
     fetches: {
       fullUrl: process.env.NODE_ENV !== "production",
--- src/app/[[...catchAll]]/route.ts
+++ src/app/[[...catchAll]]/route.ts
@@ -1,6 +1,13 @@
+import { track } from "@vercel/analytics/server";
+import { unstable_after as after } from "next/server";
+
 import { getWikipediaContent } from "@/lib/wikipedia";
 
 export async function GET() {
+  after(async () => {
+    await track("pageview");
+  });
+
   const data = await getWikipediaContent({ titles: "Jason_Derulo" });
 
   return Response.json(data);
